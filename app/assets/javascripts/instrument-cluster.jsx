42a530b484491ed9cd126827c8229f3f27e487de
Guillermo Vargas <guillermo@bstelpaso.com>
2017-05-07 23:29:36 -0600

    Adding initial state

--- app/assets/javascripts/instrument-cluster.jsx
+++ app/assets/javascripts/instrument-cluster.jsx
@@ -10,12 +10,34 @@ var WeatherInstruments = React.createClass({
 
   // Initial state of the component with an empty array of resource
   getInitialState() {
-    return { weatherData: [] }
+    return {
+      weatherData: {
+        "current_conditions_metadata": {
+          "timestamp": "2012-04-23T18:25:43.511Z",
+          "wind_speed": 2.5,
+          "wind_gusts": 10
+        },
+        "anemometer": {
+          "speed": 0,
+          "gusts": 0
+        },
+        "thermometer": {
+          "current": 27
+        },
+        "hygrometer": {
+          "relative_humidity": 0.4
+        },
+        "barometer": {
+          "pressure": 29.92,
+          "altitude": 3940
+        }
+      }
+    }
   },
   // Function executed when the component just mounted
   componentDidMount() {
     this._loadWeatherData();
-    setInterval(this._loadResources, 30000);
+    setInterval(this._loadResources, 1000);
   },
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -37,7 +59,7 @@ var Gauge = React.createClass({
       <div className="col-md-3 gauge blue">
         <div className="instrument-name">Anemometer</div>
         <div className="latest-conditions">
-          <div className="value">10</div>
+          <div className="value">{this.props.data.anemometer.speed}</div>
           <div className="units">m/s</div>
         </div>
         <h4>Current</h4>
